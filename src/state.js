6d305c74e8b3cab7a4e819d936dec32f8732ecbe
bmehder <bmehder@gmail.com>
2021-12-10 23:21:06 +0000

    set score (sans tiebreak)

--- src/state.js
+++ src/state.js
@@ -1,17 +1,17 @@
 export const match = {
-	currentSet: 1,
-	score: {
-		player1: {
-			set1: 0,
-			set2: 0,
-			set3: 0,
-			game: '0'
-		},
-		player2: {
-			set1: 0,
-			set2: 0,
-			set3: 0,
-			game: '0'
-		},
-	}
+  currentSet: 1,
+  score: {
+    player1: {
+      set1: 3,
+      set2: 0,
+      set3: 0,
+      game: '0',
+    },
+    player2: {
+      set1: 3,
+      set2: 0,
+      set3: 0,
+      game: '0',
+    },
+  },
 }
