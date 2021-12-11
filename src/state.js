628dde383650a5f285ad199a1aa65997d013c728
bmehder <bmehder@gmail.com>
2021-12-11 02:16:50 +0000

    reset score

--- src/state.js
+++ src/state.js
@@ -2,7 +2,7 @@ export const match = {
   currentSet: 1,
   score: {
     player1: {
-      set1: 4,
+      set1: 0,
       set2: 0,
       set3: 0,
       game: '0',
@@ -10,7 +10,7 @@ export const match = {
       tiebreak: 0,
     },
     player2: {
-      set1: 4,
+      set1: 0,
       set2: 0,
       set3: 0,
       game: '0',
